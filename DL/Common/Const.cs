291b028e839cf96bc9cb053cfc8d1374019bbeba
Бисер Борисов <biser@Adminsoft.local>
2019-06-19 16:48:25 +0300

    Get Connection String from appsettings.json

--- DL/Common/Const.cs
+++ DL/Common/Const.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace DL.Common
 {
     public class Const
     {
-        public static string ConnectionString = @"Server=.;Database=DL;Trusted_Connection=True;";
+        public static readonly string ConnectionString = GetConnectionString();
+        
+        private static string GetConnectionString()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+
+            configurationBuilder.AddJsonFile("appsettings.json", optional: false);
+
+            var configuration = configurationBuilder.Build();
+
+            return configuration.GetConnectionString("DefaultConnection");
+        }
     }
 }
